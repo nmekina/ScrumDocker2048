fec735ae50efae171c8075631a5a55de18dbd00f
Christian42 <cchimani@htl-steyr.ac.at>
2022-12-21 09:17:38 +0100

    a

--- src/main/java/com/example/scrumdocker2048/Controller/StartmenueController.java
+++ src/main/java/com/example/scrumdocker2048/Controller/StartmenueController.java
@@ -1,6 +1,4 @@
 package com.example.scrumdocker2048.Controller;
-
-import com.example.scrumdocker2048.Model.ChangeScene;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
@@ -20,6 +18,12 @@ public class StartmenueController extends AbstractController {
     }
 
     public void startgamebtnac(ActionEvent actionEvent) throws IOException {
-        ChangeScene.ChangeSceneNow("username", startgamebtn);
+        try {
+             UsernameController u = this.loadFxmlFile("username.fxml", "Spiel",
+                    ((Button) actionEvent.getSource()).getScene().getWindow(), UsernameController.class);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
