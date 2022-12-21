04b3843e39dbd2679ae15075d940bcdeea66b2a1
Christian42 <cchimani@htl-steyr.ac.at>
2022-12-21 08:44:16 +0100

    a

--- src/main/java/com/example/scrumdocker2048/Controller/StartmenueController.java
+++ src/main/java/com/example/scrumdocker2048/Controller/StartmenueController.java
@@ -1,5 +1,25 @@
 package com.example.scrumdocker2048.Controller;
 
+import com.example.scrumdocker2048.Model.ChangeScene;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class StartmenueController extends AbstractController {
 
+    public Button startgamebtn;
+
+    public void settingsbtnac(ActionEvent actionEvent) {
+    }
+
+    public void helpbtnac(ActionEvent actionEvent) {
+    }
+
+    public void highscorebtnac(ActionEvent actionEvent) {
+    }
+
+    public void startgamebtnac(ActionEvent actionEvent) throws IOException {
+        ChangeScene.ChangeSceneNow("username", startgamebtn);
+    }
 }
---
+++ src/main/java/com/example/scrumdocker2048/Model/ChangeScene.java
@@ -0,0 +1,37 @@
+package com.example.scrumdocker2048.Model;
+
+import com.example.scrumdocker2048.Controller.StartmenueController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class ChangeScene {
+
+    /**
+     * wechelt die Scene
+     *
+     * @param game   => Name des Fensters inn welches gewechselt werden soll
+     * @param node => Button, um die alte Stage schließen zu können
+     * @throws IOException
+     */
+    public static void ChangeSceneNow(String game, Node node) throws IOException {
+        Stage stage = new Stage();
+
+        Stage stageclose = (Stage) node.getScene().getWindow();
+        stageclose.close();
+
+        final FXMLLoader fxmlLoader = new FXMLLoader();
+        URL u = StartmenueController.class.getResource(game + ".fxml");
+
+        fxmlLoader.setLocation(u);
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle(game);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
