9b91ede5dea1bbd11167a7b1a56fbd3835cbba19
skimeswe <skimeswe@htl-steyr.ac.at>
2023-02-01 08:36:15 +0100

    style.css

--- src/main/java/com/example/scrumdocker2048/Controller/AbstractController.java
+++ src/main/java/com/example/scrumdocker2048/Controller/AbstractController.java
@@ -12,6 +12,18 @@ import java.io.IOException;
 public abstract class AbstractController {
     private Stage stage = null;
 
+    /**
+     * Loads an FXML file, instantiates a controller object, and shows a new stage.
+     * The stage can be set as modal with respect to a specified owner window.
+     *
+     * @param path              The path to the FXML file
+     * @param title             The title of the new stage
+     * @param owner             The owner window of the new stage
+     * @param classOfController The class of the controller object
+     * @param <T>               The type of the controller object
+     * @return The instantiated controller object
+     * @throws IOException if an I/O error occurs while loading the FXML file
+     */
     public <T extends AbstractController> T loadFxmlFile(String path, String title, Window owner, Class<T> classOfController) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AbstractController.class.getResource(path));
         Parent scene = fxmlLoader.load();
@@ -34,6 +46,11 @@ public abstract class AbstractController {
         return controller;
     }
 
+    /**
+     * Sets the stage of the controller object.
+     *
+     * @param s The stage to be set
+     */
     public void setStage(Stage s) {
         this.stage = s;
     }
